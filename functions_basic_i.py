e6f73a8d55ff140fb82ed51edd51c60042362bb2
antran1245 <antran1245@gmail.com>
2022-04-03 14:26:26 -0700

    completed

--- functions_basic_i.py
+++ functions_basic_i.py
@@ -1,116 +1,116 @@
 #1
-# def number_of_food_groups():
-#     return 5
-# print(number_of_food_groups())
-# # Log 5
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+# Log 5
 
 # #2
-# def number_of_military_branches():
-#     return 5
-# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-# # Error because of no function call number_of_days_in_a_week_silicon_or_triangle_sides()
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# Error because of no function call number_of_days_in_a_week_silicon_or_triangle_sides()
 
 # #3
-# def number_of_books_on_hold():
-#     return 5
-#     return 10
-# print(number_of_books_on_hold())
-# # Log 5
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+# Log 5
 
 
 # #4
-# def number_of_fingers():
-#     return 5
-#     print(10)
-# print(number_of_fingers())
-# # Log 5 causue never reach print(10)
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+# Log 5 causue never reach print(10)
 
 
 # #5
-# def number_of_great_lakes():
-#     print(5)
-# x = number_of_great_lakes()
-# print(x)
-# # Log 5 then Log nothing
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+# Log 5 then Log nothing
 
 # #6
-# def add(b,c):
-#     print(b+c)
-# print(add(1,2) + add(2,3))
-# # Log 3, Log 5, Log Error
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+# Log 3, Log 5, Log Error
 
-# #7
-# def concatenate(b,c):
-#     return str(b)+str(c)
-# print(concatenate(2,5))
-# # Log 25
+#7
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+# Log 25
 
 # #8
-# def number_of_oceans_or_fingers_or_continents():
-#     b = 100
-#     print(b)
-#     if b < 10:
-#         return 5
-#     else:
-#         return 10
-#     return 7
-# print(number_of_oceans_or_fingers_or_continents())
-# # Log 100 , Log 10
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+# Log 100 , Log 10
 
 # #9
-# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-#     if b<c:
-#         return 7
-#     else:
-#         return 14
-#     return 3
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-# # Log 7, Log 14, Log 21
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# Log 7, Log 14, Log 21
 
 # #10
-# def addition(b,c):
-#     return b+c
-#     return 10
-# print(addition(3,5))
-# # Log 8
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+# Log 8
 
 # #11
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-# print(b)
-# foobar()
-# print(b)
-# # Log 500, Log 500, Log 300, Log 500
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+# Log 500, Log 500, Log 300, Log 500
 
 # #12
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-#     return b
-# print(b)
-# foobar()
-# print(b)
-# # Log 500, Log 500, Log 300, Log 500
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+# Log 500, Log 500, Log 300, Log 500
 
 
 # #13
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-#     return b
-# print(b)
-# b=foobar()
-# print(b)
-# # Log 500, Log 500, Log 300, Log 300
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+# Log 500, Log 500, Log 300, Log 300
 
 # #14
 def foo():
@@ -124,13 +124,14 @@ foo()
 
 
 # #15
-# def foo():
-#     print(1)
-#     x = bar()
-#     print(x)
-#     return 10
-# def bar():
-#     print(3)
-#     return 5
-# y = foo()
-# print(y)
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+# Log 1, Log 3, Log 5, Log 10
