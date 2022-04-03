5c2e4355385556e8e8de6d133bd888807ba14c72
antran1245 <antran1245@gmail.com>
2022-04-03 14:21:42 -0700

    predict 12

--- functions_basic_i.py
+++ functions_basic_i.py
@@ -77,26 +77,27 @@
 # # Log 8
 
 # #11
-b = 500
-print(b)
-def foobar():
-    b = 300
-    print(b)
-print(b)
-foobar()
-print(b)
-# Log 500, Log 500, Log 300, Log 500
-
-# #12
 # b = 500
 # print(b)
 # def foobar():
 #     b = 300
 #     print(b)
-#     return b
 # print(b)
 # foobar()
 # print(b)
+# # Log 500, Log 500, Log 300, Log 500
+
+# #12
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+# Log 500, Log 500, Log 300, Log 500
 
 
 # #13
