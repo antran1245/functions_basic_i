c2758823e35815035556c84d1acd475799fda99c
antran1245 <antran1245@gmail.com>
2022-04-03 14:18:56 -0700

    predict 10

--- functions_basic_i.py
+++ functions_basic_i.py
@@ -58,23 +58,23 @@
 # # Log 100 , Log 10
 
 # #9
-def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-    if b<c:
-        return 7
-    else:
-        return 14
-    return 3
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-# Log 7, Log 14, Log 21
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# # Log 7, Log 14, Log 21
 
 # #10
-# def addition(b,c):
-#     return b+c
-#     return 10
-# print(addition(3,5))
-
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+# Log 8
 
 # #11
 # b = 500
